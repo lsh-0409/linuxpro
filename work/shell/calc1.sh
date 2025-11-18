2dadc4516fe2d22bf596a579d025d6fbde1060b6
lim <sspooding@naver.com>
2025-11-18 11:19:59 +0900

    Add: shell dir

--- work/shell/calc1.sh
+++ work/shell/calc1.sh
@@ -4,9 +4,8 @@ num1=$2
 num2=$3
 result=0
 
-
-if [[ $# -ne 3 ]]; then 
-	echo "입력: $(basename $0) [+][-][\*][/] 숫자 숫자 " >&2
+if [[ $# -ne 3 ]]; then
+	echo "입력은 $(basename $0) [+][-][\*][/] 숫자 숫자" >&2
 	exit 1
 fi
 
@@ -14,14 +13,14 @@ case $oper in
 	+) result=$((num1 + num2)) ;;
 	-) result=$((num1 - num2)) ;;
 	\*) result=$((num1 * num2)) ;;
-	/)
+	/) 
 		if [[ $num2 -eq 0 ]]; then
-			echo "0으로 나눌 수 없음 " >&2
-			exit 1 
-		fi 
+			echo "0 으로 나눌 수 없음." >&2
+			exit 1
+		fi
 		result=$((num1 / num2)) ;;
-		
-	*) echo "연산자 다시 입력: [+][-][\+][/] " >&2
+	
+	*) echo "연산자 다시 입력: [+][-][\*][/] " >&2
 	exit 1
 esac
 
